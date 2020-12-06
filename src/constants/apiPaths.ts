302cf1c81041103ef940bb35418df35212a54e9b
Izzat_Fatullayev <Izzat_Fatullayev@epam.com>
2020-12-06 22:18:34 +0300

    refactor: updated cart url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,7 +5,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5y33w7z3kk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://gblhr1x62d.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://xuatxu4bq6.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://xuatxu4bq6.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
