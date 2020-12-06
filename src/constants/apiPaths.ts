50624aadbfb7adbdb1393acd7f441f0ef38166a9
Izzat_Fatullayev <Izzat_Fatullayev@epam.com>
2020-12-06 21:38:43 +0300

    feat: added cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -5,7 +5,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://5y33w7z3kk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://gblhr1x62d.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://izzetbek-cart-api-dev.eu-west-1.elasticbeanstalk.com',
 };
 
 export default API_PATHS;
